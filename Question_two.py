94b59697e9db1d50dbadbb03f7c1817f682e3a53
omertalmi5 <omertalmi5@gmail.com>
2022-12-30 17:02:45 +0200

    add question 2

--- Question_two.py
+++ Question_two.py
@@ -23,6 +23,16 @@ def measure_inserts_in_begining():
         end = time.time()
         print((end - start)/amount)
 
+def measure_inserts_in_begining_array():
+    lst = list()
+    for i in range(0,10):
+        print("index " + str(i + 1) + " rotations: ")
+        amount = 1500 * (2 ** (i + 1))
+        start = time.time()
+        insert_to_list_n_times(lst, amount, 0)
+        end = time.time()
+        print((end - start)/amount)
+
 
 def measure_inserts_in_end():
     lst = AVLTreeList()
@@ -31,6 +41,19 @@ def measure_inserts_in_end():
         amount = 1500 * (2 ** (i + 1))
         start = time.time()
         index_to_insert = lst.getSize()-1
+        if index_to_insert == -1:
+            index_to_insert = 0
+        insert_to_list_n_times(lst, amount, index_to_insert)
+        end = time.time()
+        print((end - start)/amount)
+
+def measure_inserts_in_end_in_list():
+    lst = list()
+    for i in range(0,10):
+        print("index " + str(i + 1) + " rotations: ")
+        amount = 1500 * (2 ** (i + 1))
+        start = time.time()
+        index_to_insert = len(lst) - 1
         if index_to_insert == -1:
             index_to_insert = 0
         insert_to_list_n_times(lst, amount, index_to_insert)
@@ -47,6 +70,19 @@ def measure_inserts_in_random():
         insert_to_list_n_times_in_random(lst, amount)
         end = time.time()
         print((end - start)/amount)
+
+def measure_inserts_in_random_in_arr():
+    lst = list()
+    for i in range(0,10):
+        print("index " + str(i + 1) + " rotations: ")
+        amount = 1500 * (2 ** (i + 1))
+        start = time.time()
+        insert_to_list_n_times_in_random(lst, amount)
+        end = time.time()
+        print((end - start)/amount)
 # measure_inserts_in_begining()
 # measure_inserts_in_end()
-measure_inserts_in_random()
+# measure_inserts_in_random()
+# measure_inserts_in_begining_array()
+# measure_inserts_in_end_in_list()
+measure_inserts_in_random_in_arr()
